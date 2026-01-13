c1af265bc43e5ab7f66c23c9dfca6af6e8b2e7cf
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-01-13 15:58:33 -0300

    feat: add correção link whatsap

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/loglud.png", // imagem que aparecerá no WhatsApp
+        url: "/logoludlink.png", // imagem que aparecerá no WhatsApp
         width: 1200,
         height: 630,
       },
